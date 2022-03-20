163eca7dbda46ec253caeea958922b94f053645c
sh <oper6909@gmail.com>
2022-03-20 17:20:45 +0900

    no error

--- rust-basic/src/main.rs
+++ rust-basic/src/main.rs
@@ -1,10 +1,3 @@
-
 fn main() {
-    let tweet = Tweet {
-        username: String::from("Lee"),
-        content: String::from("Hello world, mom, dad"),
-        reply: false,
-        retweet: false,
-    };
-    println!("New Tweet! =====\n{}", tweet.summary());
+    println!("Hello world");
 }
